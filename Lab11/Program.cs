9550f1c5b573c96bd5485dc436209237379b06e1
Zakhar Galanin <zakhargalanin@gmail.com>
2022-07-03 19:56:45 -0700

    Add project files.

---
+++ Lab11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    internal class Program
+    {
+        /* Разработать структуру для решения линейного уравнения 0=kx+b. 
+         * Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. 
+         * Для решения уравнения предусмотреть метод Root. 
+         * Создать экземпляр разработанной структуры. 
+         * Осуществить использование экземпляра в программе.
+         */
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите коэффициенты линейного уравнения вида 0=kx+b. \nВведите k: ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Solution solution = new Solution(k, b);
+            Console.WriteLine(solution.Root());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lab11/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    struct Solution
+    {
+        double k;
+        double b;
+
+        public Solution (double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k == 0)
+                return "Ошибка";
+            double r = -b / k;
+            return $"Решение {r}";
+
+        }
+    }
+}
